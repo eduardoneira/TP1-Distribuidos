9c718e502bd1b8c57cfa8ea8a04762e5a4662fc7
eduardoneira <edun327@gmail.com>
2017-03-27 11:56:05 -0300

    arreglado el random posta

--- src/cliente.c
+++ src/cliente.c
@@ -67,7 +67,7 @@ int main(int argc, char** argv) {
 		Mensaje_gustos msg_gustos;
 		Mensaje_ticket msg_ticket;
 
-		crear(&msg_gustos,MENSAJE_A_CAJERO,pid,generarNumeroRandom(CANTIDAD_GUSTOS,time(NULL)),generarNumeroRandom(CANTIDAD_GUSTOS),generarNumeroRandom(CANTIDAD_GUSTOS));
+		crear(&msg_gustos,MENSAJE_A_CAJERO,pid,generarNumeroRandom(CANTIDAD_GUSTOS,pid),generarNumeroRandom(CANTIDAD_GUSTOS),generarNumeroRandom(CANTIDAD_GUSTOS));
 		char buffer[BUFFER_SIZE];
 
 		//Hago el pedido
